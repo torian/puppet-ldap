4b82260267c4b050e711441cf3204678b3319831
Emiliano Castagnari <ecastag@gmail.com>
2013-04-17 08:07:38 -0300

    Documentation

--- manifests/server/master.pp
+++ manifests/server/master.pp
@@ -1,3 +1,134 @@
+# == Class: ldap::master
+#
+# Puppet module to manage server configuration for
+# **OpenLdap**.
+#
+#
+# === Parameters
+#
+#  [suffix]
+#    
+#    **Required**
+#
+#  [rootpw]
+#    
+#    **Required**
+#
+#  [rootdn]
+#    
+#    *Optional* (defaults to 3)
+#
+#  [schema_inc]
+#    
+#    *Optional* (defaults to 30)
+#    
+#  [modules_inc]
+#    
+#    *Optional* (defaults to 30)
+#
+#  [index_inc]
+#    
+#    *Optional* (defaults to 30)
+#    
+#  [log_level]
+#    
+#    *Optional* (defaults to false)
+#    
+#  [bind_anon]
+#    
+#    *Optional* (defaults to false)
+#    
+#  [ssl]
+#    
+#    *Requires*: ssl_cert parameter
+#    *Optional* (defaults to false)
+#    
+#  [ssl_cert]
+#    
+#    *Optional* (defaults to false)
+#    
+#  [ssl_ca]
+#    
+#    *Optional* (defaults to false)
+#    
+#  [ssl_cert]
+#    
+#    *Optional* (defaults to false)
+#    
+#  [ssl_key]
+#    
+#    *Optional* (defaults to false)
+#    
+#  [syncprov]
+#    
+#    *Optional* (defaults to false)
+#    
+#  [syncprov_checkpoint]
+#    
+#    *Optional* (defaults to false)
+#    
+#  [syncprov_sessionlog]
+#    
+#    *Optional* (defaults to *'uid'*)
+#    
+#  [sync_binddn]
+#    
+#    *Optional* (defaults to *'member'*)
+#    
+#  [enable_motd]
+#    Use motd to report the usage of this module.
+#    *Requires*: https://github.com/torian/puppet-motd.git
+#    *Optional* (defaults to false)
+#    
+#  [ensure]
+#    *Optional* (defaults to 'present')
+#
+#
+# == Tested/Works on:
+#   - Debian: 5.0   / 6.0   /
+#   - RHEL    5.2   / 5.4   / 5.5   / 6.1   / 6.2 
+#   - OVS:    2.1.1 / 2.1.5 / 2.2.0 / 3.0.2 /
+#
+#
+# === Examples
+#
+# class { 'ldap':
+#	uri  => 'ldap://ldapserver00 ldap://ldapserver01',
+#	base => 'dc=suffix',
+# }
+#
+# class { 'ldap':
+#	uri  => 'ldap://ldapserver00',
+#	base => 'dc=suffix',
+#	ssl  => true,
+#	ssl_cert => 'ldapserver00.pem'
+# }
+#
+# class { 'ldap':
+#	uri        => 'ldap://ldapserver00',
+#	base       => 'dc=suffix',
+#	ssl        => true,
+#	ssl_cert => 'ldapserver00.pem'
+#
+#	nsswitch   => true,
+#	nss_passwd => 'ou=users',
+#	nss_shadow => 'ou=users',
+#	nss_group  => 'ou=groups',
+#
+#	pam        => true,
+# }
+#
+#
+# === Authors
+#
+# Emiliano Castagnari ecastag@gmail.com (a.k.a. Torian)
+#
+#
+# === Copyleft
+#
+# Copyleft (C) 2012 Emiliano Castagnari ecastag@gmail.com (a.k.a. Torian)
+#
+#
 
 class ldap::server::master($suffix, $rootpw,
 	$rootdn              = "cn=admin,${suffix}",
